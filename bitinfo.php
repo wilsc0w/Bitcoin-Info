4a27b65efec2ece19bb1ecae55a4994969aecb51
Wilson <44921584+wilsc0w@users.noreply.github.com>
2019-01-25 13:59:35 +0100

    Create bitinfo.php

---
+++ bitinfo.php
@@ -0,0 +1,27 @@
+<?php 
+/* coded by Wilson (wilsc0w on github) :) ♥ */
+@$btcadress = $argv[1];
+function multiexplode ($delimiters,$string) {
+
+    $ready = str_replace($delimiters, $delimiters[0], $string);
+    $launch = explode($delimiters[0], $ready);
+    return  $launch;
+}
+$api = "https://chain.api.btc.com/v3/address/$btcadress";
+$kxk = @file_get_contents($api);
+if(preg_match("/tx_count/",$kxk)) {
+$plodexk = multiexplode(array(":",",","}",'"'),$kxk);
+	echo "\n ~ BitcoinInfo coded by {wilc0w} on github ~\n";
+	echo "\n Bitcoin Adress : $btcadress\n";
+	echo " Transactions : $plodexk[28]\n";
+	echo " BTC's received : " . sprintf('%.8f', $plodexk[16]/100000000)."\n";
+	echo " BTC's sent : " . sprintf('%.8f', $plodexk[20]/100000000)."\n";
+	echo " Balance : " . sprintf('%.8f', $plodexk[24]/100000000). " BTC's\n\n";
+	}
+if(preg_match("/bad params/",$kxk)) {
+	echo "\nInvalid BTC Address\n\n";
+}
+elseif(!isset($btcadress)) {
+echo "\n/// php $argv[0] Btc-Address\n\n";
+}
+?>
